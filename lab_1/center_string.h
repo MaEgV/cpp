2dca0d263026ceebd003b1a942adb1c8c667656f
MaEgV <60978225+MaEgV@users.noreply.github.com>
2020-03-25 14:17:24 +0300

    Create center_string.h

---
+++ lab_1/center_string.h
@@ -0,0 +1,15 @@
+#ifndef CENTER_STRING
+#define CENTER_STRING
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <iterator>
+#include <sstream>
+
+using namespace std;
+
+void Proccess(vector<string>& words, int max_size, vector<string>& str_list);
+
+void PrintAllString(vector<string>& a_vec, const char* file_name);
+#endif // !1
