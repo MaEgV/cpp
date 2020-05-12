acd5f3b9fc16e494a8cf4df2bce7a865f5f7426d
MaEgV <60978225+MaEgV@users.noreply.github.com>
2020-05-12 09:54:19 +0300

    Update main.cpp

--- lab_3/main.cpp
+++ lab_3/main.cpp
@@ -3,13 +3,21 @@
 #include <fstream>
 
 
+int mode(int argc, char** argv){
+	if(argc == 0)
+		return 1;
+	else
+		return 2;
+}
+
+
 int main(int argc, char** argv) {
 	string str;
 	ifstream in("menu.txt");
 	getline(in, str);
 	in.close();
 	
-	launch_menu(argc, argv, str);
+	launch_menu(mode(argc, argv), str);
 
 	return 0;
 }
